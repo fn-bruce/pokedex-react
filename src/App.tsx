b6961de764d5f8de21ae666ee1784f2a2c4913ba
Bruce Nguyen <bruceleenguyen95@gmail.com>
2024-09-13 13:40:25 -0600

    feat: added shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 function App(): React.ReactElement {
   return (
-    <div className="h-screen w-screen flex justify-center items-center">
+    <div className="h-screen w-screen flex flex-col gap-4 justify-center items-center">
       <h1>Welcome to the Pokedex!</h1>
+      <Button onClick={() => alert("hello!")}>Click me!</Button>
     </div>
   );
 }
