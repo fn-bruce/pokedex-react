c76e72a3c6c01d691eff26616056ebf0cc24f106
Bruce Nguyen <bruceleenguyen95@gmail.com>
2024-09-13 12:04:11 -0600

    feat: initialized project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App(): React.ReactElement {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <h1>Welcome to the Pokedex!</h1>
+    </div>
+  );
+}
+
+export default App;
