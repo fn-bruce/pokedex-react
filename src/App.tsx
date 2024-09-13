0bd6695b4b38ae64ae67077ff06147632d93fb3d
Bruce Nguyen <bruceleenguyen95@gmail.com>
2024-09-13 14:07:17 -0600

    refactor: extracted pokemon code into component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { ModeToggle } from "./components/mode-toggle";
+import { Pokedex } from "./components/pokedex";
 
 function App(): React.ReactElement {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="h-screen w-screen flex flex-col gap-4 justify-center items-center">
-        <div className="absolute right-8 top-8">
-          <ModeToggle />
-        </div>
-        <h1>Welcome to the Pokedex!</h1>
+      <div className="absolute right-8 top-8">
+        <ModeToggle />
       </div>
+      <Pokedex />
     </ThemeProvider>
   );
 }
---
+++ src/components/pokedex.tsx
@@ -0,0 +1,7 @@
+export function Pokedex(): React.ReactElement {
+  return (
+    <div className="h-screen w-screen flex flex-col gap-4 justify-center items-center">
+      <h1>Welcome to the Pokedex!</h1>
+    </div>
+  );
+}
