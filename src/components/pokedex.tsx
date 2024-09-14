85df174648275e820c2ea655a7a0b44aeca3d6e0
Bruce Nguyen <bruceleenguyen95@gmail.com>
2024-09-14 11:57:57 -0600

    feat: added searching

--- src/components/pokedex.tsx
+++ src/components/pokedex.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -5,51 +6,69 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import usePokemonService from "@/hooks/use-pokemon-service";
 import { useToast } from "@/hooks/use-toast";
-import { LoaderCircle } from "lucide-react";
-import { useEffect, useState } from "react";
+import { LoaderCircle, Search, Shuffle } from "lucide-react";
+import { useState } from "react";
 
 export function Pokedex(): React.ReactElement {
-  const [allPokemon, setAllPokemon] = useState<Pokemon[] | null>(null);
+  const [pokemon, setPokemon] = useState<Pokemon[] | null>(null);
+  const [searchInput, setSearchInput] = useState("");
+
   const { error, loading, getAllPokemon } = usePokemonService({});
 
   const { toast } = useToast();
 
-  useEffect((): void => {
-    getAllPokemon()
-      .then((p: Pokemon[] | null): void => {
-        setAllPokemon(p);
-        toast({
-          description: "Successfully fetched all pokemon",
-          duration: 3000,
-        });
-      })
-      .catch(() => {
-        toast({
-          title: "Error",
-          description: error?.message,
-          duration: 3000,
-          variant: "destructive",
-        });
+  const handleSearch = async (): Promise<void> => {
+    try {
+      const allPokemon = await getAllPokemon();
+      console.log(searchInput);
+      const filteredPokemon =
+        allPokemon?.filter((p: Pokemon): boolean => {
+          return p.name.includes(searchInput);
+        }) ?? null;
+      console.log(filteredPokemon);
+      setPokemon(filteredPokemon);
+      toast({
+        description: `Found ${filteredPokemon?.length} Pokémon`,
+        duration: 3000,
       });
-  }, []);
+    } catch {
+      toast({
+        title: "Error",
+        description: error?.message,
+        duration: 3000,
+        variant: "destructive",
+      });
+    }
+  };
 
   return (
     <div className="h-screen w-screen flex flex-col gap-4 items-center mt-8">
       <h1 className="text-4xl bold">Pokédex</h1>
+      <div className="flex gap-4">
+        <Input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.currentTarget.value)}
+          placeholder="Search for a pokemon..."
+        />
+        <Button disabled={loading} onClick={handleSearch}>
+          <Search />
+        </Button>
+        <Button disabled={loading}>
+          <Shuffle />
+        </Button>
+      </div>
       {loading && (
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
           <LoaderCircle className="animate-spin w-36 h-36" />
         </div>
       )}
-      <div className="flex flex-wrap gap-4 m-4 justify-center">
-        {allPokemon?.map(
+      <div className="flex flex-wrap w-full gap-4 m-4 justify-center">
+        {pokemon?.map(
           (p: Pokemon, index: number): React.ReactElement => (
-            <Card
-              key={index}
-              className="w-full sm:w-1/4 flex flex-col items-center"
-            >
+            <Card key={index} className="w-1/4 flex flex-col items-center">
               <CardHeader>
                 <CardTitle>{p.name}</CardTitle>
                 <CardDescription className="text-center">
