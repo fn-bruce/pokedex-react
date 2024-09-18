06d3745206d718aa8dad1d7ac9cd8330876935a5
Bruce Nguyen <bruceleenguyen95@gmail.com>
2024-09-18 14:09:54 -0600

    fix: fixed ref issue

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,52 +1,48 @@
 import { Search as SearchIcon, Shuffle } from "lucide-react";
-import { ChangeEvent, KeyboardEvent, useState } from "react";
+import { ChangeEvent, KeyboardEvent, useState, forwardRef } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
 interface SearchProps {
-  ref: React.RefObject<HTMLInputElement>;
   loading: boolean;
   onChange: (newValue: string) => void;
   onSearch: () => Promise<void>;
   onShuffle: () => Promise<void>;
 }
-const Search = ({
-  ref,
-  loading,
-  onChange,
-  onSearch,
-  onShuffle,
-}: SearchProps): React.ReactElement => {
-  const [input, setInput] = useState("");
 
-  const handleKeyUp = (e: KeyboardEvent<HTMLInputElement>): void => {
-    if (e.key === "Enter") {
-      onSearch();
-    }
-  };
+const Search = forwardRef<HTMLInputElement, SearchProps>(
+  ({ loading, onChange, onSearch, onShuffle }, ref): React.ReactElement => {
+    const [input, setInput] = useState("");
 
-  const handleOnChange = (e: ChangeEvent<HTMLInputElement>): void => {
-    setInput(e.currentTarget.value);
-    onChange(e.currentTarget.value);
-  };
+    const handleKeyUp = (e: KeyboardEvent<HTMLInputElement>): void => {
+      if (e.key === "Enter") {
+        onSearch();
+      }
+    };
 
-  return (
-    <div className="flex gap-4">
-      <Input
-        ref={ref}
-        value={input}
-        onChange={handleOnChange}
-        onKeyUp={handleKeyUp}
-        placeholder="Search for a pokemon..."
-      />
-      <Button disabled={loading} onClick={onSearch}>
-        <SearchIcon />
-      </Button>
-      <Button disabled={loading} onClick={onShuffle}>
-        <Shuffle />
-      </Button>
-    </div>
-  );
-};
+    const handleOnChange = (e: ChangeEvent<HTMLInputElement>): void => {
+      setInput(e.currentTarget.value);
+      onChange(e.currentTarget.value);
+    };
+
+    return (
+      <div className="flex gap-4">
+        <Input
+          ref={ref}
+          value={input}
+          onChange={handleOnChange}
+          onKeyUp={handleKeyUp}
+          placeholder="Search for a pokemon..."
+        />
+        <Button disabled={loading} onClick={onSearch}>
+          <SearchIcon />
+        </Button>
+        <Button disabled={loading} onClick={onShuffle}>
+          <Shuffle />
+        </Button>
+      </div>
+    );
+  },
+);
 
 export default Search;
