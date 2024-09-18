62b3a4eac425043459650f90b94b68d0ae755bf4
Bruce Nguyen <bruceleenguyen95@gmail.com>
2024-09-18 12:24:44 -0600

    feat: added caching if we already fetched all pokemon

--- src/hooks/use-pokemon-service.ts
+++ src/hooks/use-pokemon-service.ts
@@ -9,15 +9,17 @@ export interface PokemonServiceHookResult {
 }
 
 export default function usePokemonService({}: PokemonServiceProps): PokemonServiceHookResult {
+  const [allPokemon, setAllPokemon] = useState<Pokemon[] | null>(null);
   const [error, setError] = useState<Error | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
 
   const getAllPokemon = async (): Promise<Pokemon[] | null> => {
     setLoading(true);
     try {
+      if (allPokemon) return allPokemon;
       const url = "https://pokeapi.co/api/v2/pokemon?limit=10000&offset=0";
       const response: GetAllPokemon.Response = await (await fetch(url)).json();
-      const pokemon: Pokemon[] = response.results.map((r) => {
+      const fetchedPokemon: Pokemon[] = response.results.map((r) => {
         const name = r.name;
         const urlParts = r.url.split("/");
         const idAsString = urlParts[urlParts.length - 2];
@@ -29,7 +31,8 @@ export default function usePokemonService({}: PokemonServiceProps): PokemonServi
           spriteUrl,
         };
       });
-      return pokemon;
+      setAllPokemon(fetchedPokemon);
+      return fetchedPokemon;
     } catch (err) {
       setError(err as Error);
       throw err as Error;
